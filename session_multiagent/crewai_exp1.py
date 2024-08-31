73e88450e641df69149f44ae0d1b4524f9cc8858
qayyumu <mrusmanqayyum@gmail.com>
2024-08-31 17:27:08 +0500

    Ollama agent and multiagent code

--- session_multiagent/crewai_exp1.py
+++ session_multiagent/crewai_exp1.py
@@ -4,26 +4,51 @@ import os
 os.environ["OPENAI_API_KEY"] = "NA"
 
 llm = ChatOllama(
-    model = "llama3.1",
+    #model = "llama3.1",
+    model = "phi3:mini",
     base_url = "http://localhost:11434")
 
-general_agent = Agent(role = "Math Professor",
-                      goal = """Provide the solution to the students that are asking mathematical questions and give them the answer.""",
-                      backstory = """You are an excellent math professor that likes to solve math questions in a way that everyone can understand your solution""",
+general_agent = Agent(role = "Deep learning Professor",
+                      goal = """Provide the solution to the students that are asking machine learning and deep learning questions and give them the answer.""",
+                      backstory = """You are an excellent AI/Machine learning/Deep learning professor that likes to give AI/ML/DL questions in a way that everyone can understand""",
                       allow_delegation = False,
                       verbose = True,
                       llm = llm)
 
-task = Task(description="""what is 3 + 5""",
-             agent = general_agent,
-             expected_output="A numerical answer.")
+if (0):   ##without gradio
+    
+    task = Task(description="""what is self supervised learning""",
+                agent = general_agent,
+                expected_output="A brief answer with 50-80 words.")
+    crew = Crew(
+                agents=[general_agent],
+                tasks=[task],
+                verbose=True
+            )
+    result = crew.kickoff()
+    print(result)
 
-crew = Crew(
-            agents=[general_agent],
-            tasks=[task],
-            verbose=True
-        )
+else:
+    import gradio as gr
+    def query(payload):
+        task = Task(description=payload,
+                agent = general_agent,
+                expected_output="A brief answer with 50-80 words.")
+        crew = Crew(
+                    agents=[general_agent],
+                    tasks=[task],
+                    verbose=True
+                )
 
-result = crew.kickoff()
+        result = crew.kickoff()
+        
+        return result
 
-print(result)
+    def query_fn(input_text):
+        resp = query(input_text)
+        print(resp.raw)
+        return str(resp.raw)
+
+    demo = gr.Interface(fn=query_fn, inputs="text", outputs="label")
+
+    demo.launch()
---
+++ session_multiagent/ollama_agent.py
@@ -0,0 +1,93 @@
+import requests, json
+
+import gradio as gr
+
+
+# model = 'llama3.1' #heavy model from meta 8B
+model = 'phi3:mini'  #lightweight 3B model
+context = [] 
+
+
+
+import gradio as gr
+
+
+
+#Call Ollama API
+def generate(prompt, context, top_k, top_p, temp):
+    r = requests.post('http://localhost:11434/api/generate',
+                     json={
+                         'model': model,
+                         'prompt': prompt,
+                         'context': context,
+                         'options':{
+                             'top_k': top_k,
+                             'temperature':top_p,
+                             'top_p': temp
+                         }
+                     },
+                     stream=False)
+    r.raise_for_status()
+
+ 
+    response = ""  
+
+    for line in r.iter_lines():
+        body = json.loads(line)
+        response_part = body.get('response', '')
+        #print(response_part)
+        if 'error' in body:
+            raise Exception(body['error'])
+
+        response += response_part
+
+        if body.get('done', False):
+            context = body.get('context', [])
+            return response, context
+
+
+
+def chat(input, chat_history, top_k, top_p, temp):
+
+    chat_history = chat_history or []
+
+    global context
+    output, context = generate(input, context, top_k, top_p, temp)
+
+    chat_history.append((input, output))
+  
+
+    return chat_history, chat_history
+ 
+
+#########################Gradio Code##########################
+block = gr.Blocks()
+
+
+with block:
+
+    gr.Markdown("""<h1><center> Pk ChatBot </center></h1>""")
+
+    chatbot = gr.Chatbot()
+    message = gr.Textbox(placeholder="Type here")
+    
+    state = gr.State()
+    with gr.Row():
+        top_k = gr.Slider(0.0,100.0, label="top_k", value=40, info="Reduces the probability of generating nonsense. A higher value (e.g. 100) will give more diverse answers, while a lower value (e.g. 10) will be more conservative. (Default: 40)")
+        top_p = gr.Slider(0.0,1.0, label="top_p", value=0.9, info=" Works together with top-k. A higher value (e.g., 0.95) will lead to more diverse text, while a lower value (e.g., 0.5) will generate more focused and conservative text. (Default: 0.9)")
+        temp = gr.Slider(0.0,2.0, label="temperature", value=0.8, info="The temperature of the model. Increasing the temperature will make the model answer more creatively. (Default: 0.8)")
+
+
+    # submit = gr.Button("SEND")
+    
+    def clear_input():
+        return ""
+
+    # submit.click(chat, inputs=[message, state, top_k, top_p, temp], outputs=[chatbot, state])
+    # submit.click(clear_input, inputs=None, outputs=message)
+    
+    message.submit(chat, inputs=[message, state, top_k, top_p, temp], outputs=[chatbot, state])
+    message.submit(clear_input, inputs=None, outputs=message)
+  
+
+block.launch(debug=True)
