94125affd010d847da70a71a012a43ddf524d699
qayyumu <mrusmanqayyum@gmail.com>
2024-09-02 19:05:35 +0500

    NN added for logistic regression

--- session_01/Logistic_regression.py
+++ session_01/Logistic_regression.py
@@ -4,6 +4,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import confusion_matrix,accuracy_score
+
+
+def plot_boundary(X_set,Y_set, clf):
+    #Visualizing Testing Set Results
+    from matplotlib.colors import ListedColormap
+    colors =["green", "orange", "gold", "blue", "k", "purple","red"]
+
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                        np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                                        alpha = 0.75, cmap = ListedColormap(colors))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate (np.unique(Y_set)):
+        plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                    c = ListedColormap(colors)(i), label = j)
+    plt.title('Classify users if they buy the Car')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
@@ -23,38 +46,38 @@ X_test = sc.transform(X_test)
 #Fitting Logistic Regression to the Training Set
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-classifier = SVC() #LogisticRegression()
+classifier = LogisticRegression()    ###SVC()
 classifier.fit(X_train, Y_train)
 
 #Predicting the Test set results
 Y_pred = classifier.predict(X_test)
 
 #Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix,accuracy_score
 cm = confusion_matrix(Y_test, Y_pred)
 print(cm) #shows the number of correct predictions made
 result_acc = accuracy_score(Y_test, Y_pred)
 print(result_acc)
 
-#Visualizing Testing Set Results
-from matplotlib.colors import ListedColormap
-colors =["green", "orange", "gold", "blue", "k", "purple","red"]
-X_set, Y_set = X_test, Y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                    np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-                                       alpha = 0.75, cmap = ListedColormap(colors))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate (np.unique(Y_set)):
-    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
-                c = ListedColormap(colors)(i), label = j)
-plt.title('Classify users if they buy the Car')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+plot_boundary(X_test, Y_test, classifier)
 
 
+
+from sklearn.neural_network import MLPClassifier
+classifier_nn = MLPClassifier(solver='lbfgs', alpha=1e-4,
+                    hidden_layer_sizes=(5, 2), random_state=1)
+
+classifier_nn.fit(X_train, Y_train)
+
+#Predicting the Test set results
+Y_pred = classifier_nn.predict(X_test)
+
+#Making the Confusion Matrix
+cm = confusion_matrix(Y_test, Y_pred)
+print(cm) #shows the number of correct predictions made
+result_acc = accuracy_score(Y_test, Y_pred)
+print(result_acc)
+
+
+plot_boundary(X_test, Y_test, classifier_nn)
 
 
